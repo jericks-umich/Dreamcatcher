195dbcf39a6dd8666bc8a286b7d830b2e79d952c
Jeremy Erickson <jericks@umich.edu>
2016-06-01 14:20:06 -0400

    added config file lock code

--- dreamcatcher/src/config.c
+++ dreamcatcher/src/config.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <fcntl.h>
 
 #include <uci.h>
 
@@ -49,8 +50,51 @@ void* monitor_config(void* threadid) {
   pthread_exit(NULL);
 }
 
+// return fd if successful, -1 if failed
+// block until we get a handle
+int lock_open_config() {
+  // open file to get file descriptor
+  fd = open(CONFIG_FILE, O_RDWR);
+  if (fd == -1){
+    return -1;
+  }
 
+  // acquire file lock
+  fl.l_type = F_WRLCK;
+  fl.l_whence = SEEK_SET;
+  fl.l_start = 0;
+  fl.l_len = 0;
 
+  // set a lock on the file and wait if it's already locked
+  if (fcntl(fd, F_SETLKW, &fl) == -1) { // if the lock fails
+    close(fd);
+    return -1;
+  } 
+  // if it succeeds and we have a write lock
+  return fd;
+  // leave fd and fl global variables set for subsequent unlock call
+}
+
+int unlock_close_config() {
+  int ret_val = 0;
+
+  // update file lock
+  fl.l_type = F_UNLCK;
+  fl.l_whence = SEEK_SET;
+  fl.l_start = 0;
+  fl.l_len = 0;
+
+  // unlock file
+  if (fcntl(fd, F_SETLKW, &fl) == -1) {
+    printf("Error unlocking file.\n");
+    retval = -1;
+  }
+
+  // close file descriptor
+  close(fd);
+
+  return retval;
+}
 
 
 
--- dreamcatcher/src/config.h
+++ dreamcatcher/src/config.h
@@ -4,4 +4,8 @@
 void clean_config();
 void* monitor_config(void* threadid);
 
+// global vars
+int fd;
+struct flock fl;
+
 #endif // DREAMCATCHER_CONFIG_H
