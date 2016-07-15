c064cdd864a9557d07eb98384c844b8594c847b0
Jeremy Erickson <jericks@umich.edu>
2016-07-15 16:48:58 -0400

    added end message to build-dreamcatcher.sh

--- build-dreamcatcher.sh
+++ build-dreamcatcher.sh
@@ -9,3 +9,7 @@ pushd $OPENWRT_DIR
 make package/dreamcatcher/clean
 make package/dreamcatcher/compile V=s
 popd
+
+echo "If you see build errors, fix them and try again."
+echo "If the build worked, you can get the dreamcatcher binary at"
+echo "$OPENWRT_DIR/build_dir/target-mips_34kc_musl-1.1.14/dreamcatcher/dreamcatcher"
